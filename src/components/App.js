530d71f217fbfbf721a2c5389f004a3546e0b544
Megan McCarty <megan_mccarty@hotmail.com>
2021-06-15 08:51:31 -0400

    commit tree 6a5dd15a0e822a139c63f9c071c915b8d6ae3e3a

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,26 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
+    const [items, setItems] = useState(itemData);
+    const [isDark, setIsDark] = useState(false);
 
-  // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
 
-  return (
-    <div className={"App " + (false ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
-  );
+    function handleDarkMode() {
+        setIsDark((isDark) => !isDark);
+    }
+
+    // this data will be passed down to the ShoppingList as a prop
+    console.log(items);
+
+    return (
+        <div className={"App " + (isDark ? "dark" : "light")}>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={handleDarkMode}>Dark Mode</button>
+            </header>
+            <ShoppingList items={items} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+    const [isInCart, setIsInCart] = useState(false);
+
+    function addToCart() {
+        setIsInCart((isInCart) => !isInCart);
+    }
+
+    return (
+        <li className={isInCart ? "in-cart" : null }>
+            <span>{name}</span>
+            <span className="category">{category}</span>
+            <button className="add" onClick={addToCart}>Add to Cart</button>
+        </li>
+    );
 }
 
 export default Item;
